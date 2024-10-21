36f707319fc9c182074246840390e5b7054325ec
marcosgama <marcosdanielgama@gmail.com>
2024-10-21 19:24:25 -0300

    fix leafnode

--- src/leafnode.py
+++ src/leafnode.py
@@ -4,16 +4,16 @@ from src.htmlnode import HTMLNode
 class LeafNode(HTMLNode):
     def __init__(
         self,
+        tag: str | None,
         value: str,
-        tag: str | None = None,
         props: dict[str, str] | None = None,
     ) -> None:
         super().__init__(tag, value, None, props)
 
     def __repr__(self):
-        return "LeafNode(self.value, self.tag, self.props)"
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
     def to_html(self):
         if self.tag is None:
-            return r"{self.value}"
+            return rf"{self.value}"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
